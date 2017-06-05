78d92e95f8f6f711c745af56ed1ec713fe49ba4e
GuaiYiHu <guaiyihu@foxmail.com>
2017-06-05 10:27:54 +0800

    leo: add missing makefile includes
    Change-Id: I2495cc321f74299846a4a907283316708705915e

--- mokee.mk
+++ mokee.mk
@@ -4,6 +4,10 @@ PRODUCT_RELEASE_NAME := leo
 # Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/leo/device_leo.mk)
 
